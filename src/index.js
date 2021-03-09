b49c343c8734c9c48fb156bfe93180c272e886e3
guiialmeiida <gui.almeidacrz@gmail.com>
2021-03-09 08:22:27 -0300

    Commit inicial

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { v4: uuidV4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidV4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
+});
+
+app.listen(3333);
