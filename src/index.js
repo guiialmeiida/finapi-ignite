bbfdce11fe0b153a9acf4d4e09050df06c5ff07d
guiialmeiida <gui.almeidacrz@gmail.com>
2021-03-10 07:24:37 -0300

    Validando cpf e conta e listando extrato

--- src/index.js
+++ src/index.js
@@ -9,16 +9,35 @@ const customers = [];
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
-    const id = uuidV4();
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" });
+    }
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidV4(),
         statement: []
     });
 
     return response.status(201).send();
+});
+
+app.get("/statement", (request, response) => {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found" });
+    }
+
+    return response.json(customer.statement);
 });
 
 app.listen(3333);
